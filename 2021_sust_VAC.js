ff8e398b59d308b8c4ed21c974a574552a464a4f
Katerina Krizova <krizovakk@gmail.com>
2021-04-21 15:35:20 +0200

    S1 and S2 indices calculation - general scripts

--- 2021_sust_VAC.js
+++ 2021_sust_VAC.js
@@ -1,30 +1,63 @@
 /*
-Sentinel-2 / European Space Agency
-multispectral imagery
-spectral indices: NDVI, NDWI, LAI
-workflow:
-  load collection,
-  calculate specific index over entire collection,
-  stack all dates in a single multiband image,
-  export image to Google Drive
+Vaclav
+Slovec 2014-2020
+3 typy hnoje (kráva, prase, slepice)
+NeOsol
+-> MDPI Sustainability
+RS data: S2: NDVI, NDWI, LAI
 */
 
-// IMPORT AOIs
 
-var aoi = ee.FeatureCollection('users/krizovakk/bacina21_polygon')
-var geom = ee.FeatureCollection('users/krizovakk/bacina21_point')
+var geom = ee.FeatureCollection('users/krizovakk/slovec')
 
-Map.addLayer(aoi)
+// Map.addLayer(pt); // adds points to iteractive map
 Map.addLayer(geom)
+Map.centerObject(geom)
 
 // SENTINEL2 L2A COLLECTION
 
 var collection = ee.ImageCollection("COPERNICUS/S2_SR") // S2_SR = L2A level
   .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE", 10))
-  .filterDate("2020-03-1", "2020-08-31")
-  .filterBounds(aoi)
+  .filterDate("2015-04-1", "2020-08-31")
+  .filterBounds(geom)
+  //.map(maskS2clouds);// 15 elements
 
-print(collection, 'S2collection');
+print(collection);
+
+// RGB composite
+
+var generate_rgb = function(scene) {
+  var dateString = ee.Date(scene.get('system:time_start')).format('yyyyMMdd');
+  var selected = scene.select(['B2', 'B3', 'B4']);
+  var rgb = selected.expression('b(2)+b(1)+b(0)');
+  return rgb.rename(dateString);
+};
+
+var RGBcollection = collection.map(generate_rgb);
+
+print(RGBcollection, 'RGBcollection')
+
+// STACK BASIC COLLECTION
+
+var stackCollection = function(collection) {
+  var first = ee.Image(collection.first()).select([]);
+  var appendBands = function(image, previous) {
+    return ee.Image(previous).addBands(image);
+  };
+  return ee.Image(collection.iterate(appendBands, first));
+};
+var stacked = stackCollection(RGBcollection);
+print('stacked', stacked);
+
+Map.addLayer(stacked.select(0).clip(geom), {min:-1, max:1}, 'stacked');
+
+Export.image.toDrive({
+ image: stacked,
+ description: 'basic',
+ scale: 10,
+ region: geom,
+ maxPixels: 1e9
+});
 
 // CALCULATE NDVI
 
@@ -39,7 +72,7 @@ var NDVIcollection = collection.map(calculateNDVI);
 
 print(NDVIcollection, 'NDVIcollection')
 
-// stack imagery into a single multiple-band image
+// STACK
 
 var stackCollection = function(collection) {
   var first = ee.Image(collection.first()).select([]);
@@ -51,15 +84,15 @@ var stackCollection = function(collection) {
 var stackedNDVI = stackCollection(NDVIcollection);
 print('stackedNDVI', stackedNDVI);
 
-Map.addLayer(stackedNDVI.select(0).clip(aoi), {min:-1, max:1}, 'stackedNDVI');
+Map.addLayer(stackedNDVI.select(0).clip(geom), {min:-1, max:1}, 'stackedNDVI');
 
-// export stacked
+// EXPORT STACKED
 
 Export.image.toDrive({
  image: stackedNDVI,
  description: 'NDVI',
  scale: 10,
- region: aoi,
+ region: geom,
  maxPixels: 1e9
 });
 
@@ -78,20 +111,20 @@ var NDWIcollection = collection.map(calculateNDWI);
 
 print(NDWIcollection, 'NDWI collection')
 
-// stack NDWI
+// STACK
 
 var stackedNDWI = stackCollection(NDWIcollection);
 print('stackedNDWI', stackedNDWI);
 
-Map.addLayer(stackedNDWI.select(0).clip(aoi), {min:-1, max:1}, 'stackedNDWI');
+Map.addLayer(stackedNDWI.select(0).clip(geom), {min:-1, max:1}, 'stackedNDWI');
 
-// export stacked NDWI
+// EXPORT STACKED
 
 Export.image.toDrive({
  image: stackedNDWI,
  description: 'NDWI',
  scale: 10,
- region: aoi,
+ region: geom,
  maxPixels: 1e9
 });
 
@@ -110,19 +143,19 @@ var LAIcollection = collection.map(calculateLAI);
 
 print(LAIcollection, 'LAI collection')
 
-// stack LAI
+// STACK
 
 var stackedLAI = stackCollection(LAIcollection);
 print('stackedLAI', stackedLAI);
 
-Map.addLayer(stackedLAI.select(0).clip(aoi), {min:-1, max:1}, 'stackedLAI');
+Map.addLayer(stackedLAI.select(0).clip(geom), {min:-1, max:1}, 'stackedLAI');
 
-// export stacked LAI
+// EXPORT STACKED
 
 Export.image.toDrive({
  image: stackedLAI,
  description: 'LAI',
  scale: 10,
- region: aoi,
+ region: geom,
  maxPixels: 1e9
 });
---
+++ S1_indices_calculation
@@ -0,0 +1,103 @@
+/*
+Sentinel-1 / European Space Agency
+SAR imagery
+spectral indices: RVI4S1
+workflow:
+  load separate collections for ASCENDING and DESCENDING paths,
+  calculate specific index over entire collection,
+  stack all dates in a single multiband image,
+  export image to Google Drive
+*/
+
+var aoi = ee.FeatureCollection('users/krizovakk/COV_32633')
+
+Map.addLayer(aoi)
+
+// SENTINEL-1 COLLECTIONS
+
+var asc = ee.ImageCollection('COPERNICUS/S1_GRD')
+  .filterDate("2020-03-1", "2020-08-31")
+  .filter(ee.Filter.eq('instrumentMode', 'IW'))
+  .filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'))
+  .filterBounds(aoi)
+
+print(asc, 'Sentinel-1 ASC collection');
+
+var dsc = ee.ImageCollection('COPERNICUS/S1_GRD')
+  .filterDate("2020-03-1", "2020-08-31")
+  .filter(ee.Filter.eq('instrumentMode', 'IW'))
+  .filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'))
+  .filterBounds(aoi)
+
+print(dsc, 'Sentinel-1 DSC collection');
+
+// CALCULATE RVI4S1 RADAR INDEX
+// formula:  (4xVH)/(VV+VH)
+
+// ascending path
+
+var calculateRVI = function(asc) {
+  // get a string representation of the date.
+  var dateString = ee.Date(asc.get('system:time_start')).format('yyyyMMdd');
+  var selected = asc.select('VH', 'VV');
+  var rvi = selected.expression('4*b(0)/(b(1)+b(0))');
+  return rvi.rename(dateString);
+};
+
+var ascRVIcollection = asc.map(calculateRVI);
+
+print(ascRVIcollection, 'ascRVI collection')
+
+var stackCollection = function(collection) {
+  var first = ee.Image(collection.first()).select([]);
+  var appendBands = function(image, previous) {
+    return ee.Image(previous).addBands(image);
+  };
+  return ee.Image(collection.iterate(appendBands, first));
+};
+var stackedRVIasc = stackCollection(ascRVIcollection);
+print('stackedRVIasc', stackedRVIasc);
+
+Map.addLayer(stackedRVIasc.select(0).clip(aoi), {min:-1, max:1}, 'stackedRVIasc');
+
+Export.image.toDrive({
+ image: stackedRVIasc,
+ description: 'ascRVI',
+ scale: 10,
+ region: aoi,
+ maxPixels: 1e9
+});
+
+// descending path
+
+var calculateRVI = function(dsc) {
+  // get a string representation of the date.
+  var dateString = ee.Date(dsc.get('system:time_start')).format('yyyyMMdd');
+  var selected = dsc.select('VH', 'VV');
+  var rvi = selected.expression('4*b(0)/(b(1)+b(0))');
+  return rvi.rename(dateString);
+};
+
+var dscRVIcollection = dsc.map(calculateRVI);
+
+print(dscRVIcollection, 'dscRVI collection')
+
+var stackCollection = function(collection) {
+  var first = ee.Image(collection.first()).select([]);
+  var appendBands = function(image, previous) {
+    return ee.Image(previous).addBands(image);
+  };
+  return ee.Image(collection.iterate(appendBands, first));
+};
+var stackedRVIdsc = stackCollection(dscRVIcollection);
+print('stackedRVIdsc', stackedRVIdsc);
+
+Map.addLayer(stackedRVIdsc.select(0).clip(aoi), {min:-1, max:1}, 'stackedRVIdsc');
+
+Export.image.toDrive({
+ image: stackedRVIdsc,
+ description: 'dscRVI',
+ scale: 10,
+ region: aoi,
+ maxPixels: 1e9
+});
